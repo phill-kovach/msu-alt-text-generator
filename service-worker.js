f8905dd5a9f66811cf411a6523e8b6d57b978e7a
phill-kovach <phillkovachcomposing@gmail.com>
2025-08-14 09:52:38 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// A basic service worker for caching static assets for offline use.
+const CACHE_NAME = 'msu-alt-text-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js',
+  'https://marvel-b1-cdn.bc0a.com/f00000000271534/brand.missouristate.edu/_Files/logos-m.png'
+];
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Delete old caches
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
